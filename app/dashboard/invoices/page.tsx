dcf798801f681486cce7f54a98ae3bdef180c34a
Nhat-Cao <nhatcp2000@gmail.com>
2024-08-19 13:31:50 +0700

    finish with auth and metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,12 +1,16 @@
 import Pagination from '@/app/ui/invoices/pagination'
-import { fetchInvoicesPages } from '@/app/lib/data';
+import { fetchInvoicesPages } from '@/app/lib/data'
 import Search from '@/app/ui/search'
 import Table from '@/app/ui/invoices/table'
 import { CreateInvoice } from '@/app/ui/invoices/buttons'
 import { lusitana } from '@/app/ui/fonts'
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons'
 import { Suspense } from 'react'
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+	title: 'Invoices',
+}
 
 export default async function Page({
 	searchParams,
@@ -18,7 +22,7 @@ export default async function Page({
 }) {
 	const query = searchParams?.query || ''
 	const currentPage = Number(searchParams?.page) || 1
-	const totalPages = await fetchInvoicesPages(query);
+	const totalPages = await fetchInvoicesPages(query)
 
 	return (
 		<div className='w-full'>
